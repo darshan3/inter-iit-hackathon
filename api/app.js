57234ace6f9e2e60769fdaa825c448c378cae8c1
ydidwania <didwaniayashvardhan+github@gmail.com>
2018-12-19 11:38:43 +0530

    add api

---
+++ api/app.js
@@ -0,0 +1,157 @@
+var express = require('express');
+var _ = require('lodash');
+var require = require('requirejs');
+var app = express();
+var body_parser = require('body-parser');
+
+// Use middleware to set the default Content-Type
+app.use(function (req, res, next) {
+    res.header('Content-Type', 'application/json');
+    next();
+});
+app.use(body_parser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+    
+app.get('/',function(req,res){
+    res.sendFile('index.html');
+});
+
+var player_uuid = [];
+var playerNames = [];
+var events = [];
+var game ;
+// = { status: 'distribute',
+//   currentPlay: 0,
+//   currentValidCards: [ 27, 31, 7, 23, 51, 2, 34, 50, 13, 41, 5, 44, 24 ],
+//   players:
+//    [ [ 27, 31, 7, 23, 51, 2, 34, 50, 13, 41, 5, 44, 24 ],
+//      [ 15, 35, 14, 30, 6, 22, 10, 1, 17, 25, 4, 20, 12 ],
+//      [ 39, 3, 43, 47, 26, 18, 46, 33, 49, 40, 28, 32, 36 ],
+//      [ 19, 11, 42, 38, 29, 45, 21, 9, 37, 0, 16, 8, 48 ] ] };
+var allReady = false;
+
+// query = { id, name }
+app.get('/api/ready', (req, res) => {
+    console.log('ready request', req.query);
+    if(game.status != 'distribute') {
+        return res.status(400).send({
+            success: false,
+            message: 'Game status is not in distribute',
+        })
+    }
+    if(!req.query.id) {
+        return res.status(400).send({
+            success: false,
+            message: 'uuid is reqd'
+        });
+    }
+    if(!req.query.name) {
+        return res.status(400).send({
+            success: false,
+            message: 'name is reqd'
+        });
+    }
+    let playerId = _.findIndex(player_uuid, (id) => {return id==req.query.id});
+    console.log(playerId);
+    if( playerId === -1 ){
+        player_uuid.push(req.query.id);
+        playerNames.push(req.query.name); //  send this to server
+        playerId = player_uuid.length - 1;
+        console.log(player_uuid);
+    }
+    if (playerId === 3){
+        // send this to server
+        allReady = true;
+    }
+
+    return res.status(200).send({
+        success: true,
+        cards: game.players[playerId],
+    });
+});
+
+app.post('/api/playCard', (req, res) => {
+    console.log('play Card post');
+    if(game.status != 'playing') {
+        return res.status(400).send({
+            success: false,
+            message: 'Game status is not in playing state',
+        })
+    }
+    if(!req.body.id) {
+        return res.status(400).send({
+            success: false,
+            message: 'uuid is reqd'
+        });
+    }
+    if(!req.body.card) {
+        return res.status(400).send({
+            success: false,
+            message: 'card played is reqd'
+        });
+    }
+    let playerId = _.findIndex(player_uuid, req.body.id);
+    if( playerId === -1 ){
+        return res.status(400).send({
+            success: false,
+            message: 'No such player exists'
+        });
+    }
+    if (game.currentPlay != playerId) {
+        return res.status(400).send({
+            success: false,
+            message: 'Not your turn yet'
+        });
+    }
+    if( _.findIndex(game.current_valid_cards, card) >= 0 ) {
+        // Queuing this event to send to server
+        events.push({
+            playerId,
+            card
+        });
+        return res.status(200).send({
+            success: true,
+            message: 'Accepted'
+        });
+    }
+    return res.status(400).send({
+        success: false,
+        message: 'Invalid Card'
+    })    
+});
+
+app.post('/api/internalPost', (req, res) => {
+    console.log('internal post');
+    console.log('...params', req.params);
+    console.log('...query', req.query);
+    game = req.body; 
+    console.log(game);
+    return res.status(200).send({
+        success: true
+    });
+})
+
+app.get('/api/internalGet', (req, res) => {
+    console.log('internal get');
+    res.status(200).send({
+        allReady,
+        playerNames,
+        events,
+    });
+    events = [];
+    return;
+
+})
+
+const PORT = 6969;
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+});
+
+
+// double play of cards
